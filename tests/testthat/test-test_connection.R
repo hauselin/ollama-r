0feb06d59c81ce6b95e21c4f8a1c8e24902dfb65
Hause Lin <hauselin@gmail.com>
2024-07-28 16:35:51 -0400

    Fix test

--- tests/testthat/test-test_connection.R
+++ tests/testthat/test-test_connection.R
@@ -2,6 +2,8 @@ library(testthat)
 library(ollamar)
 
 test_that("test_connection function works", {
+    skip_if_not(test_connection()$status_code == 200, "Ollama server not available")
+
     result <- test_connection()
     expect_s3_class(result, "httr2_response")
     expect_equal(result$status_code, 200)
